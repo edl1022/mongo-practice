35ca6dc7a16363a4bc741649341f4ef6acdbfd52
edaniels2275 <edaniels2275@gmail.com>
2019-10-01 21:34:02 -0400

    added heroku link

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://user:password1@ds141320.mlab.com:41320/heroku_6hddwhh5", { useMongoClient: true });
 
 // Routes
 
